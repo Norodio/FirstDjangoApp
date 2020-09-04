6776307a5dcbe24a4a8097e05dbd1133a68a19ce
Thomas <tom.arnaud97@gmail.com>
2020-09-04 10:50:33 +0200

    Creation basic pages

--- app/userPage/urls.py
+++ app/userPage/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name="userPage"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('connexionPage/', views.connexionPage, name='connexionPage'),
+    path('informationPage/<int:user_id>/', views.informationPage, name='informationPage')
 ]
--- app/userPage/views.py
+++ app/userPage/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import User
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    all_users = User.objects.all()[:5]
+    context = {
+        'all_users': all_users,
+    }
+    return render(request, 'userPage/index.html', context)
+
+
+def connexionPage(request):
+    return HttpResponse("You're at the connexion page")
+
+def informationPage(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    return render(request,"userPage/informationPage.html",{"user":user})
