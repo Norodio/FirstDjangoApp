f3233956005aff119a817bd5019bf534e29a9eea
Thomas <tom.arnaud97@gmail.com>
2020-09-05 15:14:37 +0200

    Cleaning

--- app/userPage/urls.py
+++ app/userPage/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name="userPage"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('connexionPage/', views.connexionPage, name='connexionPage'),
+    path('index/', views.index, name='index'),
+    path('', views.connexionPage, name='connexionPage'),
     path('informationPage/<int:user_id>/', views.informationPage, name='informationPage'),
     path('connect/', views.connect, name='connect'),
     path('update/<int:user_id>/',views.update,name='update')
